2b11ce9ed085b60b4d60244f76794475962aa35b
Oskar Wójcikiewicz <oskar@saturdaysheroes.dev>
2024-03-17 15:00:26 +0100

    feat: config

---
+++ src/config.ts
@@ -0,0 +1,46 @@
+import * as core from '@actions/core'
+import * as mustache from 'mustache'
+import * as fs from 'fs'
+
+const input = (input: string, required: boolean = false) => {
+  return core.getInput(input, { required })
+}
+
+const getPortainerConfig = () => {
+  return {
+    baseUrl: input('portainer-url', true),
+    username: input('portainer-username', true),
+    password: input('portainer-password', true),
+    endpointId: input('portainer-endpoint-id', true),
+  }
+}
+
+const getEnv = () => {
+  const raw = input('env-file', true)
+  const envs = raw.split('\n')
+  return envs.map((env) => {
+    const [name, value] = env.split('=')
+    return { name, value }
+  })
+}
+
+const getStackConfig = (feature: string) => {
+  const path = input('stack-path', true)
+  const file = fs.readFileSync(path, 'utf-8')
+  const parsed = mustache.render(file, { feature })
+
+  return {
+    name: `feature-${feature}`,
+    stack: parsed,
+    pullImage: true,
+    prune: true,
+  }
+}
+
+const config = {
+  portainer: getPortainerConfig(),
+  env: getEnv(),
+  stack: getStackConfig(input('feature', true)),
+}
+
+export default config
