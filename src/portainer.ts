367a9a89abc6418a73ebfe3c09a30e73bffc7469
Oskar Wójcikiewicz <oskar@saturdaysheroes.dev>
2024-03-17 19:41:33 +0100

    feat: portainer client

---
+++ src/portainer.ts
@@ -0,0 +1,101 @@
+import axios from 'axios'
+import type { Axios } from 'axios'
+
+interface ICreateStackConfig {
+  env: {
+    name: string
+    value: string
+  }[]
+  fromAppTemplate?: boolean
+  name: string
+  stackFileContent: string
+  webhook?: string
+}
+
+export class Portainer {
+  private readonly baseUrl: string
+  private readonly username: string
+  private readonly password: string
+  private readonly axios: Axios
+  private token: string
+
+  constructor(baseUrl: string, username: string, password: string) {
+    this.baseUrl = `${baseUrl}/api`
+    this.username = username
+    this.password = password
+    this.axios = axios.create({
+      baseURL: this.baseUrl,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    this.token = null
+  }
+
+  private async authenticate() {
+    if (this.token) return
+
+    const { data } = await this.axios.post('/auth', {
+      username: this.username,
+      password: this.password,
+    })
+
+    this.token = data.jwt
+  }
+
+  public async createStack(endpointId: number, stack: ICreateStackConfig) {
+    await this.authenticate()
+
+    const { data } = await this.axios.post(
+      '/stacks/create/standalone/string',
+      {
+        ...stack,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${this.token}`,
+        },
+        params: {
+          endpointId,
+        },
+      },
+    )
+
+    return data
+  }
+
+  public async getAllStacks(endpointId?: number) {
+    await this.authenticate()
+
+    const { data } = await this.axios.get('/stacks', {
+      headers: {
+        Authorization: `Bearer ${this.token}`,
+      },
+      params: {
+        filters: endpointId ? JSON.stringify({ endpointId }) : undefined,
+      },
+    })
+
+    return data
+  }
+
+  public async getStackByName(name: string, endpointId: number) {
+    const stacks = await this.getAllStacks(endpointId)
+    return stacks.find((stack) => stack.Name === name)
+  }
+
+  public async deleteStack(stackId: string, endpointId: string) {
+    await this.authenticate()
+
+    const { data } = await this.axios.delete(`/stacks/${stackId}`, {
+      headers: {
+        Authorization: `Bearer ${this.token}`,
+      },
+      params: {
+        endpointId,
+      },
+    })
+
+    return data
+  }
+}
